6c67ca317714f7d88393f3425b56bc61e4a7be6c
Vo Minh Tuan <tuan.vo@dsquare.com.vn>
2018-07-30 21:37:13 +0700

    add socket.io

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 const path = require("path");
+const http = require('http')
 const express = require("express");
+const socketIO = require('socket.io')
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 5000
 const app = express();
+let server = http.createServer(app)
+let io = socketIO(server)
+io.on('connection', (socket) => {
+  console.log('New user connect')
+  socket.on('disconnect' ,() => {
+    console.log('Disconnected with client')
+  })
+})
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
-  console.log("Server is Up On" + port);
+server.listen(port, () => {
+  console.log("Server is Up On " + port);
 });
