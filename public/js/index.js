5dd11276c9901e6f1adf73fb8a1aca142038fdac
Vo Minh Tuan <tuan.vo@dsquare.com.vn>
2018-08-01 14:12:18 +0700

    Emit newMessage on create message

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,6 @@
 let socket = io()
 socket.on('connect',function(){
-  console.log('Connected to server')
-  socket.emit('createMessage', {
-    from: 'tuan@gmail.com',
-    text: 'Hey, this is new message from Tuan',
-  })
+  console.log('Connected to server')  
 })
 
 socket.on('disconnect', function(){
--- server/server.js
+++ server/server.js
@@ -10,14 +10,12 @@ let io = socketIO(server)
 io.on('connection', (socket) => {
   console.log('New user connect')
 
-  socket.emit('newMessage', {
-    from: 'mike@example.com',
-    text: 'Hey. What is going on, this is new message for you',
-    createdAt: Date.now()
-  })
-
-  socket.on('createMessage', (newMessage) => {
-    console.log('createMessage ' ,newMessage)
+  socket.on('createMessage', (message) => {
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: Date.now()
+    })
   })
 
   socket.on('disconnect' ,() => {
