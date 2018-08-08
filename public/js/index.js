d1cf605a4ccc1701ca151a6b68c6d934f52a6cb6
Vo Minh Tuan <tuan.vo@dsquare.com.vn>
2018-08-08 14:58:35 +0700

    add ui

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,24 @@
 let socket = io()
-socket.on('connect',function(){
-  console.log('Connected to server')  
+socket.on('connect', function () {
+  console.log('Connected to server')
 })
 
-socket.on('disconnect', function(){
+socket.on('disconnect', function () {
   console.log('Disconnected from server')
 })
 
-socket.on('newMessage', function(message){
+socket.on('newMessage', function (message) {
   console.log('newMessage ', message)
+  let li = $(`<li></li>`)
+  li.text(`${message.from}: ${message.text}`)
+  $('#messages').append(li)
+})
+$('#message-form').on('submit', function (e) {
+  e.preventDefault()
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $(`[name=message]`).val()
+  }, function (data) {
+    console.log(data)
+  })
 })
--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-const path = require("path");
+const path = require("path")
 const http = require('http')
 const express = require("express");
 const socketIO = require('socket.io')
-const publicPath = path.join(__dirname, "../public");
+const publicPath = path.join(__dirname, "../public")
 const port = process.env.PORT || 5000
 const app = express();
 let server = http.createServer(app)
@@ -10,15 +10,29 @@ let io = socketIO(server)
 io.on('connection', (socket) => {
   console.log('New user connect')
 
-  socket.on('createMessage', (message) => {
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat App'
+  })
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New User Joined'
+  })
+  socket.on('createMessage', (message, callback) => {
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: Date.now()
     })
+    // callback && callback('This is from the server')
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: Date.now(),
+    // })
   })
 
-  socket.on('disconnect' ,() => {
+  socket.on('disconnect', () => {
     console.log('Disconnected with client')
   })
 })
@@ -26,5 +40,5 @@ io.on('connection', (socket) => {
 app.use(express.static(publicPath));
 
 server.listen(port, () => {
-  console.log("Server is Up On " + port);
-});
+  console.log("Server is Up On " + port)
+})
